ae9921e30fd215fadef463ff0a77efbfaefbbee0
도희 <63181856+dohy12@users.noreply.github.com>
2024-03-08 16:35:58 +0900

    포탈 좌표 수정

--- Assets/Resources/Scripts/MapManager.cs
+++ Assets/Resources/Scripts/MapManager.cs
@@ -58,22 +58,22 @@ public class MapManager : MonoBehaviour
         potalInfo.AddPotal(1, new Vector2(-63f, 17f), 2); //집에 들어감
         potalInfo.AddPotal(2, new Vector2(3f, 3f), 0); //집에서 나올때
 
-        potalInfo.AddPotal(3, new Vector2(-64f, 37f), 0); //방안 들어감
-        potalInfo.AddPotal(4, new Vector2(-61f, 25f), 0); //방안 나올때
+        potalInfo.AddPotal(3, new Vector2(-64f, 38f), 0); //방안 들어감
+        potalInfo.AddPotal(4, new Vector2(-61f, 26f), 0); //방안 나올때
 
         potalInfo.AddPotal(5, new Vector2(-47f, 17f), 2); //연구소 들어감
         potalInfo.AddPotal(6, new Vector2(-4f, 5f), 0); //연구소 나올때
 
-        potalInfo.AddPotal(7, new Vector2(-14f, 17f), 2); //마트1 들어감
+        potalInfo.AddPotal(7, new Vector2(-14f, 16f), 2); //마트1 들어감
         potalInfo.AddPotal(8, new Vector2(-7f, -5f), 0); //마트1 나올때
 
-        potalInfo.AddPotal(9, new Vector2(-30f, 17f), 2); //포켓몬 센터1 들어감
+        potalInfo.AddPotal(9, new Vector2(-30f, 16f), 2); //포켓몬 센터1 들어감
         potalInfo.AddPotal(10, new Vector2(0f, -5f), 0); //포켓몬 센터1 나올때
 
-        potalInfo.AddPotal(11, new Vector2(5f, 17f), 2); //체육관 들어감
+        potalInfo.AddPotal(11, new Vector2(6f, 16f), 2); //체육관 들어감
         potalInfo.AddPotal(12, new Vector2(-60f, -3f), 0); //체육관 나올때
 
-        potalInfo.AddPotal(13, new Vector2(-14f, 30f), 2); //마트2 들어감
+        potalInfo.AddPotal(13, new Vector2(-14f, 29f), 2); //마트2 들어감
         potalInfo.AddPotal(14, new Vector2(-60f, 5f), 0); //마트2 나올때
 
         potalInfo.AddPotal(15, new Vector2(-30f, 30f), 2); //포켓몬 센터2 들어감
